cd4c0a791e5ba54c7554e657425b8683fd48a8da
LoreMiranda <lorena.miranda.lagos@gmail.com>
2019-02-26 08:26:11 -0300

    HTML con estructura en proceso, CSS se agregan algunos estilos, MAIN.JS se agrega manejo del dom para que muestre la data y filtre, DATA.JS se agrega función de filtrar.

--- src/data.js
+++ src/data.js
@@ -3,8 +3,32 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
 
-window.example = example;
+const filterData = (data, condition) => {
+  const filter = data.filter(element =>{
+    return element.type.indexOf(condition) !== -1; 
+    //return element.type.includes(condition) === true;
+  })
+  //console.log(filter)
+  return filter;
+  }
+
+
+  
+
+//const sortData = (data, sortBy, sortOrder) => {
+
+//}
+
+//const computeStats = (data) => {
+
+//}
+
+//window.data = data;
+
+//window.sortData = sortData;
+//window.computeStats = computeStats;
+
+
+
+window.filterData = filterData;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,48 @@
 /* Manejo del DOM */
+const data = POKEMON.pokemon; //constante para manipular la data
+const containerRoot = document.getElementById("root"); //todo lo que haga en el dom se muestra en el html por el id root
+const showData = (data) => { //funcion para mostrar la data
+
+data.forEach(element => {
+//console.log(element.name)    
+containerRoot.innerHTML += `<p>${element.name}</p>
+<img class= "pokeImage" src="${element.img}" alt="${element.name}">`  
+    })
+}
+//filtrar
+document.getElementById("pokemontype").addEventListener("change", () => {
+    let condition = document.getElementById("pokemontype").value;
+    let result = window.filterData(data, condition);
+//console.log(result)
+    containerRoot.innerHTML = "";
+    
+
+    result.forEach(element => {
+        containerRoot.innerHTML += `<p>${element.name}</p>
+        <img class= "pokeImage" src="${element.img}" alt="${element.name}">  `
+
+    })
+});
+
+/*for (let i = 0; i < data.length; i++) {
+    console.log(data[i].name)
+    console.log(data[i].type)
+    console.log(data[i].weaknesses)
+    console.log(data[i].egg)
+}
+
+const filterData = [];
+for (let i = 0; i < data.length; i++) {
+}
+
+
+/*const sortData = sort.data ( a,  b );
+for (let i = 0; i < data.length; i++) {
+    if (data.name[i] <= a){
+
+    }
+}
+*/
+
+
+window.onload = showData(data); //no se ejecuta la función hasta que se asegure que esta todo listo
