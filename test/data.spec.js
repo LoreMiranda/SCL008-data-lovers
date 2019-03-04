dbe9301fddceff71fe94aac71c38f8e86611b2ce
Relun <claudia.araya.ro@gmail.com>
2019-03-04 13:00:25 -0300

    test funcion filtrar

--- test/data.spec.js
+++ test/data.spec.js
@@ -3,14 +3,29 @@ global.assert = require('chai').assert;
 require('../src/data');
 require('./data.spec.js');
 
+describe('Funcion filterData', () => {
+  const data = [
+    {
+  
+      name: "Squirtle",
+      type: "Water"
+      
+    },
+    {
+      name:"Dratini",
+      type:"Dragon"
 
-describe('example', () => {
+    }
+  ]
   
   it('debería ser una función', () => {
-    assert.equal(typeof example, 'function');
+    assert.deepEqual ( typeof filterData, 'function');
   });
 
-  it('debería retornar "example"', () => {
-    assert.equal(example(), 'example');
+  it('deberia retornar el objeto "Squirtle" para primer pokemon tipo "Water"', () => {
+    assert.deepEqual(window.filterData(data, "Water"), [{name:"Squirtle", type: "Water"}]);
   });
 })
+
+  
+
