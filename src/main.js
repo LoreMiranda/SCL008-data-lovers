74e0d01d7ef9b5e1a79da4e7768e129d759431a9
LoreMiranda <lorena.miranda.lagos@gmail.com>
2019-03-06 15:05:06 -0300

    TEST pasa test de filtro, solo da error los botones que no se estan usando por ahora

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 /* Manejo del DOM */
-const data = POKEMON.pokemon; //constante para manipular la data
+const data = window.POKEMON.pokemon; //constante para manipular la data
 const containerRoot = document.getElementById("root"); //todo lo que haga en el dom se muestra en el html por el id root
 const showData = (data) => { //funcion para mostrar la data
 
@@ -18,7 +18,11 @@ btnPokedex.addEventListener("click", () => {
 document.getElementById("root").innerHTML = "";
 
 data.forEach(element => {
-  containerRoot.innerHTML += `<div class="card-with-modal">
+  containerRoot.innerHTML += `
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-sm-4">
+  <div class="card-with-modal">
   <div class="card">
       <img src=${element.img} class="card-img-top" alt="${element.name}">
       <div class="card-body">
@@ -44,9 +48,14 @@ data.forEach(element => {
                         <div class="col-md-8">
                           <div class="card-body">
                             <h5 class="card-title">${element.name}</h5>
+                            <p class="card-text">N° ${element.num}</p>
                             <p class="card-text">Tipo: ${element.type}</p>
-                            <p class="card-text">Debilidades: ${element.weaknesses.join(", ")}</p>
+                            <p class="card-text">Caramelos: ${element.candy}</p>
+                            <p class="card-text">Caramelos para Evolución: ${element.candy_count}</p>
                             <p class="card-text">Aparece en huevos de: ${element.egg}</p>
+                            <p class="card-text">Siguiente Evolución: ${element.next_evolution}</p>
+                            <p class="card-text">Debilidades: ${element.weaknesses.join(", ")}</p>
+                            
                             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                           </div>
                         </div>
@@ -58,7 +67,10 @@ data.forEach(element => {
               </div>
             </div>
           </div>
-        </div>`    
+        </div>
+        </div>
+</div>
+</div>`    
       })
   });
   //FIN BOTON POKEDEX Y TEMPLATE DE LA DATA
@@ -183,6 +195,36 @@ for (let i = 0; i < data.length; i++) {
 }
 */
 
+/*
+//INICIO BOTON OBJETOS
+btnObjetos.addEventListener("click", () => {
+  document.getElementById("root").innerHTML = "";
+  
+  data.forEach(element => {
+    containerRoot.innerHTML += `
+    <p>
+  <a class="btn btn-primary" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Toggle first element</a>
+  <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Toggle second element</button>
+  <button class="btn btn-primary" type="button" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">Toggle both elements</button>
+</p>
+<div class="row">
+  <div class="col">
+    <div class="collapse multi-collapse" id="multiCollapseExample1">
+      <div class="card card-body">
+        
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="collapse multi-collapse" id="multiCollapseExample2">
+      <div class="card card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+      </div>
+    </div>
+  </div>
+</div>
+`
+*/
 
 window.onload = showData(data); 
 //no se ejecuta la función hasta que se asegure que esta todo listo
--- test/data.spec.js
+++ test/data.spec.js
@@ -3,14 +3,26 @@ global.assert = require('chai').assert;
 require('../src/data');
 require('./data.spec.js');
 
+describe('Funcion filterData', () => { // a quien voy a testear//
+ const data = [
+   {
 
-describe('example', () => {
-  
-  it('debería ser una función', () => {
-    assert.equal(typeof example, 'function');
-  });
+     name: "Squirtle",
+     type: "Water"
 
-  it('debería retornar "example"', () => {
-    assert.equal(example(), 'example');
-  });
+   },
+   {
+     name:"Dratini",
+     type:"Dragon"
+
+   }
+ ]
+
+ it('debería ser una función', () => { // que voy a testear o probar//
+   assert.deepEqual ( typeof filterData, 'function'); //assert: es quien se encarga de evaluar que el test haga lo que tiene que hacer, metodos son una funcion, ejem deepequal, esa funcion la entrega chai, en chai se ven ejemplos//
+ });
+
+ it('deberia retornar el objeto "Squirtle" para primer pokemon tipo "Water"', () => {
+   assert.deepEqual(window.filterData(data, "Water"), [{name:"Squirtle", type: "Water"}]);
+ });
 })
